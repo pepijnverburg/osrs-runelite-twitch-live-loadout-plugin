4625d488b245125d6596eaeb84ad9d3de3fb7e7b
Pepijn Verburg <pepijn@moeilijkedingen.nl>
2020-08-06 22:46:08 +0200

    fix: allowed the link script to work from the project root.

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,4 +2,4 @@
 
 # Symlink to your Runelite codebase for easy testing.
 # Make sure the second argument matches your local setup.
-ln -s $PWD/../src/main/java/com/twitchliveloadout ../osrs-runelite/runelite-client/src/main/java/net/runelite/client/plugins/twitchliveloadout
+ln -s $PWD/src/main/java/com/twitchliveloadout ../osrs-runelite/runelite-client/src/main/java/net/runelite/client/plugins/twitchliveloadout
