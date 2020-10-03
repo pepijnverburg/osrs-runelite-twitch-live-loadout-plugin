662d616a09e8405c223bc879c4c37cca5eba563d
Pepijn Verburg <pepijn@moeilijkedingen.nl>
2020-10-03 22:53:25 +0200

    fix: changed the expiry time of fights to 30 minutes instead of an hour.

--- src/main/java/com/twitchliveloadout/FightStateManager.java
+++ src/main/java/com/twitchliveloadout/FightStateManager.java
@@ -26,7 +26,7 @@ public class FightStateManager
 	public static final String HIDDEN_PLAYER_ACTOR_NAME = "__self__";
 	public static final int DEATH_ANIMATION_ID = 836;
 	public static final int MAX_FIGHT_AMOUNT = 10;
-	public static final int FIGHT_EXPIRY_TIME = 1 * 60 * 60; // seconds
+	public static final int FIGHT_EXPIRY_TIME = (int) (0.5f * 60 * 60); // seconds
 	public static final int GRAPHIC_HITSPLAT_EXPIRY_TIME = 2500; // ms, after testing a bit longer than 4 game ticks catches all hitsplats
 
 	public static final int GRAPHIC_SKILL_XP_DROP_EXPIRY_TIME = 1500; // ms, after testing they can be either -1ms or 1ms apart from each other
@@ -579,7 +579,8 @@ public class FightStateManager
 		long lastUpdate = fight.getLastUpdate();
 		long lastUpdateDelta = now - lastUpdate;
 
-		if (lastUpdateDelta > FIGHT_EXPIRY_TIME)
+		// refresh when fight is expired and the statistics will be non-representative
+		if (lastUpdate > 0 && lastUpdateDelta > FIGHT_EXPIRY_TIME)
 		{
 			deleteFight(fight);
 			createFight(actor);
