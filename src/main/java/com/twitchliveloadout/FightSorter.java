46e819b3d868632cce4e9a5281eb4b0c13dc4bf9
Pepijn Verburg <pepijn@moeilijkedingen.nl>
2020-10-17 10:33:42 +0200

    fix: fixed an issue where the fight sorter was taking into account damge / zeroes by other actors. This can cause the included fight list for the Twitch state to toggle between several fights instead of representing the ones the local player is active in.

--- src/main/java/com/twitchliveloadout/FightSorter.java
+++ src/main/java/com/twitchliveloadout/FightSorter.java
@@ -6,8 +6,8 @@ public class FightSorter implements Comparator<Fight>
 {
 	public int compare(Fight f1, Fight f2)
 	{
-		long lastUpdate = f1.getLastUpdate();
-		long comparedLastUpdate = f2.getLastUpdate();
+		long lastUpdate = f1.getLastUpdate(true);
+		long comparedLastUpdate = f2.getLastUpdate(true);
 		boolean isLater = lastUpdate > comparedLastUpdate;
 
 		// check for equal
